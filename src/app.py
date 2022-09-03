67109e746b43e138a74a4c42007b08410bf270e7
RV <89245166+devhousemxpertz@users.noreply.github.com>
2022-09-03 10:43:14 +0530

    initial launch

--- src/app.py
+++ src/app.py
@@ -7,7 +7,7 @@ import plotly.graph_objs as go
 import plotly.express as px
 #from dash.exceptions import PreventUpdate
 import pandas as pd
-import numpy as np
+# import numpy as np
 # import pathlib
 # from app import app
 
